7121c172e3f8c8ce5e6468f13b398159f0016fad
Luca Barone <luca.b@docsity.com>
2016-03-09 18:05:56 +0100

    clean task init

--- tasks/index.js
+++ tasks/index.js
@@ -2,5 +2,5 @@ import gulp from 'gulp'
 import sequence from 'gulp-sequence'
 
 gulp.task('default', sequence('build', 'watch'))
-gulp.task('build', sequence('html'))
+gulp.task('build', sequence('clean', 'html'))
 gulp.task('watch', sequence(['html:watch', 'sync']))
