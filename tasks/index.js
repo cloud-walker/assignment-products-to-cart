80f3f80179d33bb513f28160fb02847ce820beb2
Luca Barone <luca.b@docsity.com>
2016-03-09 17:31:35 +0100

    build watch task init

--- tasks/index.js
+++ tasks/index.js
@@ -1,3 +1,6 @@
 import gulp from 'gulp'
+import sequence from 'gulp-sequence'
 
-gulp.task('default', [])
+gulp.task('default', sequence('build', 'watch'))
+gulp.task('build', [])
+gulp.task('watch', [])
