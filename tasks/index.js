ba0512b65f45ee6018c2a0ad0cf69a8939493ea0
cloud-walker <baro.luc@gmail.com>
2016-03-12 16:19:22 +0100

    js init

--- tasks/index.js
+++ tasks/index.js
@@ -4,6 +4,7 @@ import sequence from 'gulp-sequence'
 gulp.task('default', sequence('build', 'watch'))
 gulp.task('build', sequence('clean', ['html', 'css', 'assets']))
 gulp.task('watch', sequence([
+	'js:watch',
 	'html:watch',
 	'css:watch',
 	'assets:watch',
---
+++ tasks/js.js
@@ -0,0 +1,48 @@
+import path from 'path'
+import gulp from 'gulp'
+import notify from 'gulp-notify'
+import source from 'vinyl-source-stream'
+import through from 'through2'
+import sync from 'browser-sync'
+import browserify from 'browserify'
+import watchify from 'watchify'
+import babelify from 'babelify'
+import {src, dest} from '../places'
+
+const entry = `${src}/index.js`
+const browserifyConfig = {
+	cache: {},
+	packageCache: {},
+	fullPaths: true,
+	debug: true
+}
+const errorHandler = notify.onError({
+	title: 'JS Error',
+	message: '<%= error %>'
+})
+
+gulp.task('js:watch', () => {
+	const b = browserify(browserifyConfig)
+
+	b.add(entry)
+
+	b.plugin(watchify)
+	b.on('update', () => generateBundle(b))
+
+	b.transform(babelify)
+
+	return generateBundle(b)
+})
+
+function generateBundle(b) {
+	return b.bundle()
+		.on('error', errorHandler)
+		.pipe(source(entry))
+		.pipe(through.obj(function (file, enc, done) {
+			file.base = path.resolve(src)
+			this.push(file)
+			done()
+		}))
+		.pipe(gulp.dest(dest))
+		.pipe(sync.stream())
+}
