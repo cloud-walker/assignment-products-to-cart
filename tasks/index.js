351f7108962a7be2824bde97e56cd9c6ac66d0c5
cloud-walker <baro.luc@gmail.com>
2016-03-17 11:02:29 +0100

    minor adjustments

--- tasks/index.js
+++ tasks/index.js
@@ -3,10 +3,9 @@ import sequence from 'gulp-sequence'
 
 gulp.task('default', sequence('build', 'watch'))
 gulp.task('build', sequence('clean', ['html', 'css', 'assets']))
-gulp.task('watch', sequence([
-	'js:watch',
+gulp.task('watch', sequence('sync', [
 	'html:watch',
 	'css:watch',
 	'assets:watch',
-	'sync'
+	'js:watch'
 ]))
