38f85c7a00a8587617706244a5654357152edf5f
Luca Barone <luca.b@docsity.com>
2016-03-18 15:11:31 +0100

    react init

--- source/index.js
+++ source/index.js
@@ -1,1 +1,7 @@
-console.log('js init')
+import React from 'react'
+import {render} from 'react-dom'
+
+const root = document.getElementById('content')
+const dom = <div>React init.</div>
+
+render(dom, root)
