02a999ca18343eaea4430e23fde93499bdd47708
cloud-walker <baro.luc@gmail.com>
2016-03-21 20:18:18 +0100

    sort by departureTime

--- source/index.js
+++ source/index.js
@@ -7,10 +7,15 @@ import {products} from './storage'
 import Main from './components/Main'
 
 const root = document.getElementById('content')
-const vdom = <Main products={products.map(x => Object.assign(x, {
-	arrivalTime: moment(x.arrivalTime).format('HH:mm'),
-	departureTime: moment(x.departureTime).format('HH:mm'),
-	duration: moment.duration(parseInt(x.duration), 'minutes').format('h[h] m[m]')
-}))} />
+const vdom = <Main products={products
+	.sort((a, b) => {
+		return moment(a.departureTime).isAfter(b.departureTime)
+	})
+	.map(x => Object.assign(x, {
+		arrivalTime: moment(x.arrivalTime).format('HH:mm'),
+		departureTime: moment(x.departureTime).format('HH:mm'),
+		duration: moment.duration(parseInt(x.duration), 'minutes').format('h[h] m[m]')
+	}))
+} />
 
 render(vdom, root)
